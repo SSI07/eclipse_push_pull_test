4bbeb0c5fbb3649e9e7b985962faaceaf350d490
SSI07 <73834171+SSI07@users.noreply.github.com>
2020-11-06 12:44:14 +0530

    Update Hello.java
    comment

--- src/com/ssi/l2c/Hello.java
+++ src/com/ssi/l2c/Hello.java
@@ -3,8 +3,8 @@ package com.ssi.l2c;
 public class Hello {
 
 	public static void main(String[] args) {
-		
- 		System.out.println("Hello World");
+		//comment
+ 		System.out.println("Hello World!!!!");
 	}
 
 }
