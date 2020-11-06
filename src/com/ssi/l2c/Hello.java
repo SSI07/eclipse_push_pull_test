5dd88d770f2627b7c28d56291751bdb14015e22d
Sheik2252 <ssirfan1999@gmail.com>
2020-11-06 22:10:52 +0530

    primitive type challenge

--- src/com/ssi/l2c/Hello.java
+++ src/com/ssi/l2c/Hello.java
@@ -1,10 +1,19 @@
 package com.ssi.l2c;
 
+import java.util.Random;
+
+import static java.lang.Math.abs;
+
 public class Hello {
 	
 	public static void main(String[] args) {
-		//comment done jdjdj
- 		System.out.println("Hello World!!!!");
+		Random rand=new Random();
+		byte val1= (byte) rand.nextInt(Byte.MAX_VALUE);
+		short val2= (short) rand.nextInt(Short.MAX_VALUE);
+		int val3= rand.nextInt(Integer.MAX_VALUE);
+		long total=50000+10*(val1+val2+val3);
+
+		System.out.println("Total: "+total);
 	}
 
 }
